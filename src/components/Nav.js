112dff549e4b9d207d20be92f1a81ec5d2f7f52b
Redha Nedjari <ned.r@hotmail.fr>
2021-02-15 04:21:57 +0100

    2 - Navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,47 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
+// assets
+import logo from "../img/logo-pinkville-hotel-300-64x38.svg";
+import { FaAlignRight } from "react-icons/fa";
 export default class Nav extends Component {
+  //State
+  state = {
+    isToggleon: false,
+  };
+  //Handlers
+  handleToggle = () => {
+    this.setState({ isToggleon: !this.state.isToggleon });
+  };
   render() {
-    return <div>NAV</div>;
+    return (
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <Link to="/">
+              <img src={logo} alt="Hotel" />
+            </Link>
+            <button
+              type="button"
+              className="nav-btn"
+              onClick={this.handleToggle}
+            >
+              <FaAlignRight className="nav-icon" />
+            </button>
+          </div>
+          <ul
+            className={
+              this.state.isToggleon ? "nav-links show-nav" : "nav-links"
+            }
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/rooms">Rooms</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
